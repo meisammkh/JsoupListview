23572e642e9fa8662a33108fd08747b15f3655e7
meisammkh <meisam.mkh@gmail.com>
2023-01-19 17:40:41 +0330

    Initial commit

---
+++ app/src/main/java/com/example/jsouplistview/CustomAdapter.java
@@ -0,0 +1,55 @@
+package com.example.jsouplistview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomAdapter extends BaseAdapter {
+
+    Context context;
+    List<String> newsList;
+    LayoutInflater inflater;
+    Typeface typeFace;
+
+    public CustomAdapter(Context ctx, List<String> newsList ){
+        this.context = ctx;
+        this.newsList = newsList;
+        inflater = LayoutInflater.from(ctx);
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return 150;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @SuppressLint("ViewHolder")
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        view = inflater.inflate(R.layout.activity_listview_detail, null);
+        TextView textView = (TextView) view.findViewById(R.id.headline);
+        typeFace = Typeface.createFromAsset(context.getAssets(), "fonts/iransans.ttf");
+        textView.setText(newsList.get(i));
+        textView.setTypeface(typeFace);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/jsouplistview/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.jsouplistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Elements news;
+    public ArrayList<String> newsList = new ArrayList<>();
+    public ArrayList<String> newsListMain = new ArrayList<>();
+    private ListView lv;
+    private CustomAdapter customAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.news_list);
+        customAdapter = new CustomAdapter(this,newsListMain);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_listview_detail, R.id.headline, newsList);
+        ArrayAdapter<String> adapterMain = new ArrayAdapter<String>(this, R.layout.activity_listview_detail, R.id.headline_main, newsListMain);
+
+        new NewThread().execute();
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent openLinksIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(newsList.get(i)));
+                startActivity(openLinksIntent);
+            }
+        });
+
+    }
+
+    public class NewThread extends AsyncTask<String, Void, String> {
+
+        ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+
+
+        @Override
+        protected void onPreExecute(){
+            dialog.setMessage("Loading News...");
+            dialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... arg) {
+
+            Document doc;
+            try {
+                doc = Jsoup.connect("https://varzesh3.com").get();
+                news = doc.select("li.text-type>a");
+                newsList.clear();
+                for (Element news : news) {
+                    newsList.add(news.attr("href"));
+                    newsListMain.add(news.text());
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+
+            lv.setAdapter(customAdapter);
+
+        }
+
+    }
+
+}
